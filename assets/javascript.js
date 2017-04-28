d0d13cc260576a737dc721bcf45cc87546e02b69
ais047 <hursungwoo@yahoo.com>
2017-04-28 15:55:03 -0700

    working code

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,58 +1,156 @@
-var wordlist = ["Suzumiya Haruhi no Yuutsu", "Guilty Crown", "Darker than Black", "Elfen Lied", 
-"Rurouni Kenshin"
-];
-var start = "";
-var rand = Math.floor((Math.random()* wordlist.length)+0);
-rand = 2;
-var guess = 10;
-var input;
+window.onload = function () {
 
-console.log(rand);
-underscorer();
+  var alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
+        'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
+        'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+  
+  var categories;         // Array of topics
+  var chosenCategory;     // Selected catagory
+  var getHint ;          // Word getHint
+  var word ;              // Selected word
+  var guess ;             // Geuss
+  var geusses = [ ];      // Stored geusses
+  var lives ;             // Lives
+  var counter ;           // Count correct geusses
+  var space;              // Number of spaces in word '-'
 
-//used only once
-function underscorer(input){
-	for(var i = 0; i < wordlist[rand].length; i++){
-		if(input === wordlist[rand].charAt(i)){
-			start += input;
-		}
-		else if(wordlist[rand].charAt(i)===" "){
-			start += " ";
-		}
-		else{
-			start += "_";
-		}
-		start += " ";
-	}
-};
+  // Get elements
+  var showLives = document.getElementById("mylives");
 
 
+  // create alphabet ul
+  var buttons = function () {
+    myButtons = document.getElementById('buttons');
+    letters = document.createElement('span');
 
-window.onload = function(){
-
-document.getElementById("word").innerHTML = ("The word is: " + start);
-
-
-document.getElementById("game").innerHTML = ("Guesses remaining: " + guess);
-
-document.onkeyup = function onkeyup(event){
-	// console.log(event.keyCode);
-	// console.log(String.fromCharCode(event.keyCode));
-	input = String.fromCharCode(event.keyCode);
-
-		underscorer(input);
-
+    for (var i = 0; i < alphabet.length; i++) {
+      letters.id = 'alphabet';
+      list = document.createElement('span');
+      list.id = 'letter';
+      list.innerHTML = alphabet[i];
+      check();
+      myButtons.appendChild(letters);
+      letters.appendChild(list);
+    }
+  }
+    
+  
 
 
-		for(var i = 0; i < wordlist[rand].length; i++){
-			if(input === wordlist[rand].charAt(i).toUpperCase()){
-				console.log("hit");
-				document.getElementById("game").innerHTML = ("Guesses remaining: " + guess);
-				
-			}
-		}
-		guess--;
-		document.getElementById("game").innerHTML = ("Guesses remaining: " + guess);
+  // Create geusses ul
+   result = function () {
+    wordHolder = document.getElementById('hold');
+    correct = document.createElement('span');
 
-	}
+    for (var i = 0; i < word.length; i++) {
+      correct.setAttribute('id', 'my-word');
+      guess = document.createElement('span');
+      guess.setAttribute('class', 'guess');
+      if (word[i] === "-") {
+        guess.innerHTML = " &nbsp; ";
+        space = 1;
+      } else {
+        guess.innerHTML = "_ ";
+      }
+
+      geusses.push(guess);
+      wordHolder.appendChild(correct);
+      correct.appendChild(guess);
+    }
+  }
+  
+  // Show lives
+   comments = function () {
+    showLives.innerHTML = "You have " + lives + " lives";
+    if (lives < 1) {
+      showLives.innerHTML = "YOU DIED";
+    }
+    for (var i = 0; i < geusses.length; i++) {
+      if (counter + space === geusses.length) {
+        showLives.innerHTML = "You Win!";
+      }
+    }
+  }
+
+
+  
+   
+
+  // OnClick Function
+   check = function () {
+    list.onclick = function () {
+      var geuss = (this.innerHTML);
+      this.setAttribute("class", "active");
+      this.onclick = null;
+      for (var i = 0; i < word.length; i++) {
+        if (word[i] === geuss) {
+          geusses[i].innerHTML = geuss;
+          counter += 1;
+        } 
+      }
+      var j = (word.indexOf(geuss));
+      if (j === -1) {
+        lives -= 1;
+        comments();
+      } else {
+        comments();
+      }
+    }
+  }
+//
+
+   	document.onkeyup = function onkeyup(event) {
+      var geuss = String.fromCharCode(event.keyCode);
+
+      for (var i = 0; i < word.length; i++) {
+        if (word[i] === geuss) {
+          geusses[i].innerHTML = geuss;
+          counter += 1;
+        } 
+      }
+      var j = (word.indexOf(geuss));
+      if (j === -1) {
+        lives -= 1;
+        comments();
+      } else {
+        comments();
+      }
+    }
+//  
+    
+  // Play
+  play = function () {
+    categories = [
+        ["DARKER THAN BLACK", "GUILTY CROWN", "ONE PIECE"],
+        ["POKEMON", "EROMANGA SENSEI", "BERSERK"],
+        ["KIMI NO NA WA", "DN ANGEL", "GUNDAM"]
+    ];
+
+    chosenCategory = categories[Math.floor(Math.random() * categories.length)];
+    word = chosenCategory[Math.floor(Math.random() * chosenCategory.length)];
+    word = word.replace(/\s/g, "-");
+    console.log(word);
+    buttons();
+
+    geusses = [ ];
+    lives = 10;
+    counter = 0;
+    space = 0;
+    result();
+    comments();
+  }
+
+  play();
+ 
+   // Reset
+
+  document.getElementById('reset').onclick = function() {
+    correct.parentNode.removeChild(correct);
+    letters.parentNode.removeChild(letters);
+    showClue.innerHTML = "";
+    context.clearRect(0, 0, 400, 400);
+    play();
+  }
 }
+
+
